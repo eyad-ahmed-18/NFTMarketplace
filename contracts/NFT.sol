71759a89dac72ccd9f4dc8ee8e2a03f48febef9a
Eyad Ahmad <eyadah18@gmail.com>
2023-03-06 20:29:13 +0300

    Add files via upload

---
+++ contracts/NFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;    
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "MET") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemID = _tokenIds.current();
+
+        _mint(msg.sender, newItemID);
+        _setTokenURI(newItemID, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemID;
+    }
+}
